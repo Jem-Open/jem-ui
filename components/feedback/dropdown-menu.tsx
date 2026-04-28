cf1ee57288a45e57ecc8e915340ddcd34ed5f86d
michellejemhr <michelle@jemhr.com>
2026-04-28 16:22:21 +0200

    rename due to clash

--- components/feedback/dropdown-menu.tsx
+++ components/feedback/dropdown-menu.tsx
@@ -34,10 +34,10 @@ function DropdownMenuTrigger({
 function DropdownMenuContent({
   className,
   sideOffset = 4,
-  fullWidth = false,
+  matchTriggerWidth = true,
   ...props
 }: React.ComponentProps<typeof DropdownMenuPrimitive.Content> & {
-  fullWidth?: boolean
+  matchTriggerWidth?: boolean
 }) {
   return (
     <DropdownMenuPrimitive.Portal>
@@ -47,7 +47,7 @@ function DropdownMenuContent({
         className={cn(
           "z-50 max-h-(--radix-dropdown-menu-content-available-height) min-w-[8rem] origin-(--radix-dropdown-menu-content-transform-origin) overflow-x-hidden overflow-y-auto rounded-lg border border-greyscale-border bg-white p-1 shadow-md",
           "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-          fullWidth && "w-[var(--radix-dropdown-menu-trigger-width)]",
+          matchTriggerWidth && "w-[var(--radix-dropdown-menu-trigger-width)]",
           className
         )}
         {...props}
